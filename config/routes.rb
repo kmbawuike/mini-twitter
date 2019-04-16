ecf62454d5baf0a6baee1546d647909488fe7fe7
kmbawuike <kmbawuike@gmail.com>
2019-04-15 23:55:26 -0400

    Integrated CRUD operations on Posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/" => "home#top"
   get "/about" => "home#about"
+
+
   get "/posts/index" => "posts#index"
+  get "/posts/new" => "posts#new"
   get "/posts/:id/" => "posts#show"
+  post "posts/create" => "posts#create"
+  get "/posts/:id/edit" => "posts#edit"
+  post "/posts/:id/update" => "posts#update"
+  post "/posts/:id/destroy" => "posts#destroy"
+
 end
