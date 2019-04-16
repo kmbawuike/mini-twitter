9dc1b9da671d1a7b6ae4c8dedcf0421313391df1
kmbawuike <kmbawuike@gmail.com>
2019-04-16 07:46:33 -0400

    Created routes for the users controllers, added validation to user model, integrated crud operation for user model

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   get "/about" => "home#about"
 
   get "/users/index" => "users#index"
+  get "/signup" => "users#new"
+  get "/users/:id" => "users#show"
+  post "/users/create" => "users#create"
+  get "/users/:id/edit" => "users#edit"
+  post "/users/:id/update" => "users#update"
 
 
   get "/posts/index" => "posts#index"
