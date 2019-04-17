54ba4e133420bd685ea3c7cd714259e8f7faf6f7
kmbawuike <kmbawuike@gmail.com>
2019-04-17 09:30:36 -0400

    Integrated password encryption to the application

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_17_103630) do
+ActiveRecord::Schema.define(version: 2019_04_17_114415) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
@@ -30,10 +30,10 @@ ActiveRecord::Schema.define(version: 2019_04_17_103630) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password"
     t.string "image_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
